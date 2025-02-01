e398ca2c06bddb071a724103a14e817c273e8170
Italo1033 <italogammy1033@gmail.com>
2025-02-01 05:39:58 -0300

    feat(agendamento): add Agendamento data for scheduling items

---
+++ src/components/DataList/data/agendamento.ts
@@ -0,0 +1,10 @@
+const Agendamento = [
+    { id: 1, data1: "10/10/2024", data2: "NOTEBOOK" },
+    { id: 2, data1: "12/10/2024", data2: "LABORÁTORIO DE INFOMARTICA" },
+    { id: 3, data1: "15/10/2024", data2: "NOTEBOOK" },
+    { id: 4, data1: "20/10/2024", data2: "DATASHOW" },
+    { id: 5, data1: "25/10/2024", data2: "NOTEBOOK" },
+    { id: 6, data1: "30/10/2024", data2: "NOTEBOOK" },
+  ];
+  
+  export default Agendamento;
