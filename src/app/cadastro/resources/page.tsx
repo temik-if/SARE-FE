74b95ef68de980dd6f5c1230459f1fb3c08ab62e
Italo1033 <italogammy1033@gmail.com>
2025-03-09 00:29:16 -0300

    feat: add registration page with styling and resource form component

---
+++ src/app/cadastro/resources/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import ResourceForm from '@/components/ResouceForm/ResourceForm';
+import styles from './page.module.css';
+
+export default function RegisterPage() {
+  return (
+    <div className={styles.container}>
+      
+      <ResourceForm />
+    </div>
+  );
+}
