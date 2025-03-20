50b267070f5625039eaea51d2eed36fb8427a21a
Italo1033 <italogammy1033@gmail.com>
2025-03-20 16:00:03 -0300

    feat(booking): update booking service endpoints for improved clarity and consistency

--- src/service/bookingService.ts
+++ src/service/bookingService.ts
@@ -6,13 +6,13 @@ export const bookingService = {
     post<IBooking>("/booking", bookingData),
   getAll: () => get<IBooking[]>("/booking"),
   getAllFromLoggedUser: () => get<IBooking[]>(`/booking/my`),
-  getByStatus: (status: string) => get<IBooking[]>(`/booking/status/${status}`),
+  getByStatus: (status: string) => get<IBooking[]>(`/booking/${status}`),
   getByShift: (shift: string) => get<IBooking[]>(`/booking/shift/${shift}`),
   getByUser: (userId: string) => get<IBooking[]>(`/booking/user/${userId}`),
   getByResource: (resourceId: number) =>
     get<IBooking[]>(`/booking/resource/${resourceId}`),
   getByDate: (date: string) => get<IBooking[]>(`/booking/date/${date}`),
-  updateStatus: (id: number, status: string) =>
+  patch: (id: number, status: string) =>
     patch(`/booking/status/${id}`, { status }),
   update: (id: number, bookingData: IBookingUpdate) =>
     put(`/booking/${id}`, bookingData),
