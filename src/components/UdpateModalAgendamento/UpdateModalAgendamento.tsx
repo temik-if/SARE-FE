69e1161ba73d4519c7bbd3b45016a5b2361b641c
Italo1033 <italogammy1033@gmail.com>
2025-03-20 16:00:08 -0300

    feat(EditModal): fetch and display user and resource names in edit modal

--- src/components/UdpateModalAgendamento/UpdateModalAgendamento.tsx
+++ src/components/UdpateModalAgendamento/UpdateModalAgendamento.tsx
@@ -6,6 +6,9 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import { userService } from "@/service/userService";
+import { resourceService } from "@/service/resourceService";
+import { MdEdit } from 'react-icons/md'; // Importando o ícone de edição
 
 interface EditModalProps {
   isOpen: boolean;
@@ -17,10 +20,12 @@ interface EditModalProps {
 export default function EditModal({ isOpen, onClose, onConfirm, bookingData }: EditModalProps) {
   const [id, setId] = useState<number | undefined>();
   const [resource_id, setResourceId] = useState<number | undefined>();
-  const [shift, setShift] = useState<'MORNING' | 'AFTERNOON' | 'EVENING' | undefined>(undefined);
+  const [shift, setShift] = useState<'MORNING' | 'AFTERNOON' | 'EVENING' | ''>('');
   const [date, setDate] = useState('');
   const [classList, setClassList] = useState<number[]>([]);
   const [status, setStatus] = useState('');
+  const [userName, setUserName] = useState('');
+  const [resourceName, setResourceName] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
@@ -29,15 +34,37 @@ export default function EditModal({ isOpen, onClose, onConfirm, bookingData }: E
     if (bookingData) {
       setId(bookingData.id);
       setResourceId(bookingData.resource_id);
-      setShift(bookingData.shift || undefined);
+      setShift(bookingData.shift || '');
       setDate(bookingData.date || '');
       setClassList(bookingData.class || []);
       setStatus(bookingData.status || '');
+
+      // Buscar o nome do usuário
+      if (bookingData.user_id) {
+        userService.getById(bookingData.user_id.toString()).then(user => {
+          setUserName(user.first_name + ' ' + user.last_name);
+        }).catch(error => {
+          console.error("Erro ao buscar usuário:", error);
+        });
+      }
+
+      // Buscar o nome do recurso
+      if (bookingData.resource_id) {
+        resourceService.getById(bookingData.resource_id.toString()).then(resource => {
+          setResourceName(resource.name);
+        }).catch(error => {
+          console.error("Erro ao buscar recurso:", error);
+        });
+      }
     }
   }, [bookingData]);
 
   const handleShiftChange = (event: SelectChangeEvent) => {
-    setShift(event.target.value as 'MORNING' | 'AFTERNOON' | 'EVENING' | undefined);
+    setShift(event.target.value as 'MORNING' | 'AFTERNOON' | 'EVENING' | '');
+  };
+
+  const handleStatusChange = (event: SelectChangeEvent) => {
+    setStatus(event.target.value);
   };
 
   const validateForm = () => {
@@ -63,16 +90,16 @@ export default function EditModal({ isOpen, onClose, onConfirm, bookingData }: E
     return Object.keys(newErrors).length === 0;
   };
 
-  const handleSave = () => {
-    if (!validateForm()) return;
+  // const handleSave = () => {
+  //   if (!validateForm()) return;
 
-    onConfirm({ id, resource_id, shift, date, class: classList, status });
-    setSuccessMessage("Agendamento atualizado com sucesso!");
-    setTimeout(() => {
-      setSuccessMessage('');
-      onClose();
-    }, 2000);
-  };
+  //   onConfirm({ id, resource_id, shift, date, class: classList, status });
+  //   setSuccessMessage("Agendamento atualizado com sucesso!");
+  //   setTimeout(() => {
+  //     setSuccessMessage('');
+  //     onClose();
+  //   }, 2000);
+  // };
 
   if (!isOpen) return null;
 
@@ -80,39 +107,108 @@ export default function EditModal({ isOpen, onClose, onConfirm, bookingData }: E
     <div className={styles.overlay}>
       <div className={styles.modal}>
         <h2>Editar Agendamento</h2>
+
+        {/* Campo para o nome do usuário */}
+        <div className={styles.field}>
+          <label>Usuário</label>
+          <div className={styles.editableField}>
+            <input
+              type="text"
+              value={userName}
+              className={`${styles.input} ${errors.userName ? styles.error : ''}`}
+              disabled
+            />
+          </div>
+          {errors.userName && <span className={styles.errorMessage}>{errors.userName}</span>}
+        </div>
+
+        {/* Campo para o nome do recurso */}
+        <div className={styles.field}>
+          <label>Recurso</label>
+          <div className={styles.editableField}>
+            <input
+              type="text"
+              value={resourceName}
+              className={`${styles.input} ${errors.resourceName ? styles.error : ''}`}
+              disabled
+            />
+          </div>
+          {errors.resourceName && <span className={styles.errorMessage}>{errors.resourceName}</span>}
+        </div>
+
+        {/* Campo para o turno */}
         <div className={styles.field}>
           <label>Turno</label>
-          <Box sx={{ minWidth: 120 }}>
-            <FormControl sx={{ width: "100%" }}>
-              <Select
-                value={shift || ''}
-                onChange={handleShiftChange}
-                input={<OutlinedInput notched={false} />}
-                sx={{ backgroundColor: "rgba(255, 255, 255, 0.8)" }}
-              >
-                <MenuItem value="MORNING">Manhã</MenuItem>
-                <MenuItem value="AFTERNOON">Tarde</MenuItem>
-                <MenuItem value="EVENING">Noite</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl sx={{ width: "100%" }}>
+                <Select
+                  labelId="shift-select-label"
+                  id="shift-select"
+                  value={shift}
+                  onChange={handleShiftChange}
+                  input={<OutlinedInput notched={false} />}
+                  sx={{
+                    backgroundColor: "rgba(255, 255, 255, 0.8)",
+                    "& .MuiOutlinedInput-notchedOutline": { borderColor: "#ddd" },
+                    "&:hover .MuiOutlinedInput-notchedOutline": { borderColor: "#ddd" },
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": { borderColor: "#ddd" },
+                  }}
+                  className={`${styles.input} ${errors.shift ? styles.error : ''}`}
+                >
+                  <MenuItem value="MORNING">Manhã</MenuItem>
+                  <MenuItem value="AFTERNOON">Tarde</MenuItem>
+                  <MenuItem value="EVENING">Noite</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
           {errors.shift && <span className={styles.errorMessage}>{errors.shift}</span>}
         </div>
 
+        {/* Campo para a data */}
         <div className={styles.field}>
           <label>Data</label>
-          <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+          <div className={styles.editableField}>
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className={`${styles.input} ${errors.date ? styles.error : ''}`}
+            />
+          </div>
           {errors.date && <span className={styles.errorMessage}>{errors.date}</span>}
         </div>
 
-        <div className={styles.field}>
+         {/* Campo para o status */}
+         <div className={styles.field}>
           <label>Status</label>
-          <input type="text" value={status} onChange={(e) => setStatus(e.target.value)} />
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl sx={{ width: "100%" }}>
+                <Select
+                  labelId="status-select-label"
+                  id="status-select"
+                  value={status}
+                  onChange={handleStatusChange}
+                  input={<OutlinedInput notched={false} />}
+                  sx={{
+                    backgroundColor: "rgba(255, 255, 255, 0.8)",
+                    "& .MuiOutlinedInput-notchedOutline": { borderColor: "#ddd" },
+                    "&:hover .MuiOutlinedInput-notchedOutline": { borderColor: "#ddd" },
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": { borderColor: "#ddd" },
+                  }}
+                  className={`${styles.input} ${errors.status ? styles.error : ''}`}
+                >
+                  <MenuItem value="IN_PROGRESS">Em Andamento</MenuItem>
+                  <MenuItem value="COMPLETED">Concluído</MenuItem>
+                  <MenuItem value="CLOSED">Fechado</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
           {errors.status && <span className={styles.errorMessage}>{errors.status}</span>}
         </div>
-
         <div className={styles.buttons}>
-          <button onClick={handleSave} className={styles.saveButton}>Salvar</button>
+          {/* <button onClick={handleSave} className={styles.saveButton}>Salvar</button> */}
+          <button className={styles.saveButton}>Salvar</button>
+
         </div>
       </div>
     </div>
